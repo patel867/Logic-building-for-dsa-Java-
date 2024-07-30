9500cd7d6fc0bea3eeee799c540d8584e5da71e3
Patel  Jay <163373450+patel867@users.noreply.github.com>
2024-07-30 14:17:56 +0530

    Add files via upload

---
+++ Rectangular Star Pattern.java
@@ -0,0 +1,17 @@
+class main {
+    static void pattern(int N)
+ {
+     for (int i = 0; i < N; i++)     // Outer  loop for the rows
+     {
+         for (int j = 0; j < N; j++)  // Inner loop for columns ,which will print star
+         {
+             System.out.print("* ");
+         }
+         System.out.println(); // To move to the nextline
+     }
+ }
+     public static void main(String[] args) {
+         int N = 5;
+         pattern(N);
+     }
+ }
